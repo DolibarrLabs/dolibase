5ad7a3228753b34f178cacca02e178e997b9c10c
AXeL-dev <anass_denna@hotmail.fr>
2018-10-17 12:39:38 +0100

    update books test module

--- test/books/class/book.class.php
+++ test/books/class/book.class.php
@@ -25,53 +25,9 @@ class Book extends CustomObject
 	public function __construct()
 	{
 		$this->fetch_fields = array('rowid', 'ref', 'name', 'desc', 'type', 'qty', 'price', 'ref', 'publication_date', 'creation_date', 'created_by', 'model_pdf');
+		$this->date_fields = array('creation_date', 'publication_date');
 
 		parent::__construct();
-	}
-
-	/**
-	 * Load object in memory from database
-	 *
-	 * @param  int     $id object Id
-	 * @param  string  $ref object ref
-	 * @return int     <0 if KO, >0 if OK
-	 */
-	public function fetch($id, $ref = '')
-	{
-		$result = parent::fetch($id, $ref);
-
-		// Fix error: dol_print_date function call with deprecated value of time
-		$this->creation_date = $this->db->jdate($this->creation_date);
-		$this->publication_date = $this->db->jdate($this->publication_date);
-
-		return $result;
-	}
-
-	/**
-	 * Load all object entries in memory from database
-	 *
-	 * @param  int     $limit        fetch limit
-	 * @param  int     $offset       fetch offset
-	 * @param  string  $sort_field   field to sort by
-	 * @param  string  $sort_order   sort order: 'DESC' or 'ASC'
-	 * @param  string  $more_fields  more fields to fetch
-	 * @param  string  $join         join clause
-	 * @param  string  $where        where clause (without 'WHERE')
-	 * @param  boolean $get_total    get total number of records or not
-	 * @param  boolean $table_alias  Alias to use for table name, leave it empty if you won't
-	 * @return int                   <0 if KO, >0 if OK
-	 */
-	public function fetchAll($limit = 0, $offset = 0, $sort_field = '', $sort_order = 'DESC', $more_fields = '', $join = '', $where = '', $get_total = false, $table_alias = 't')
-	{
-		$result = parent::fetchAll($limit, $offset, $sort_field, $sort_order, $more_fields, $join, $where, $get_total, $table_alias);
-
-		// Fix error: dol_print_date function call with deprecated value of time
-		for ($i = 0; $i < count($this->lines); $i++) { 
-			$this->lines[$i]->creation_date = $this->db->jdate($this->lines[$i]->creation_date);
-			$this->lines[$i]->publication_date = $this->db->jdate($this->lines[$i]->publication_date);
-		}
-
-		return $result;
 	}
 
 	/**
