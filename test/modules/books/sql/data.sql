e377e22f887cd1cb481012804b5f9b372a7a9af1
AXeL-dev <anass_denna@hotmail.fr>
2019-02-07 23:47:58 +0100

    test modules: remove old license code

--- test/modules/books/sql/data.sql
+++ test/modules/books/sql/data.sql
@@ -1,18 +1,5 @@
 -- <Dolibase dictionary table example>
 -- Copyright (C) <2018>  <AXeL>
---
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 INSERT INTO llx_books_dict (rowid, label, active) VALUES (1, 'Science & nature', 1);
 INSERT INTO llx_books_dict (rowid, label, active) VALUES (2, 'History', 1);
