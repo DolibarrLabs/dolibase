b31768eef93a2977f1085f6a00c8a7a71eaa43ce
AXeL-dev <contact.axel.dev@gmail.com>
2020-09-27 11:32:13 +0100

    Maintenance release
    v3.0.7

--- config.php
+++ config.php
@@ -23,7 +23,7 @@ $dolibase_path = get_dolibase_path();
 
 return array(
 	'main' => array(
-		'version'             => '3.0.6', // possible values: a.b.c-alpha, a.b.c-beta, a.b.c-rcX or a.b.c
+		'version'             => '3.0.7', // possible values: a.b.c-alpha, a.b.c-beta, a.b.c-rcX or a.b.c
 		'env'                 => 'prod', // environment: dev or prod
 		'link'                => 'https://github.com/AXeL-dev/dolibase',
 		'path'                => $dolibase_path,
